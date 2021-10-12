bfd023f3afeaff2f47bc6f52b0f0c403a4d6e57f
JesusBastidas <davidbastidascarvajal@gmail.com>
2021-10-11 21:31:39 -0500

    En esta actualizacion se incluyen los diferentes tipos de operadores

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
-let numero=5;
-console.log(numero);
+let numero=5.4;
+let palabra="hola mundo";
+let respuesta= true;
+const PI=3.14;
+console.log(numero, palabra, respuesta, PI);
+/*Operadores matematicos*/
+let a=5;
+let b=2;
+let usuario="jesus";
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log("Hola "+ usuario); /*asi se concatena un string*/
+/*Operaciones de incremento*/
+console.log(a);
+console.log(++a);/*si pongo el doble mas antes de la variable el me hace el incremento y luego me imprime el resultado*/
+console.log(--a);/*de igual forma que el comentario anterior pero con decremento*/
+/*Operadores de asignacion*/
+a+=3;
+console.log(a);/*en este caso el += funciona de la misma manera que el a=a+3*/
+a-=3;
+console.log(a);
+a*=3;
+console.log(a);
+a/=3;
+console.log(a);
+a%=3;
+console.log(a);/*como el codigo se lee de forma vertical la variable a cambia tras cada asignacion de tal suerte que el nuevo operador funciona con el nuevo valor de a en cada ocacion*/
